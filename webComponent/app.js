2a088a1b8d48186d185fdb02ad7fd97d1467ffc1
ashu <ashutoshsuthar01@gmail.com>
2023-03-24 17:25:13 +0530

    login implementation

--- webComponent/app.js
+++ webComponent/app.js
@@ -1,22 +1,32 @@
+require('dotenv').config({path: __dirname + '/.env'});
 const express			= require('express');
 const mongoose			= require('mongoose');
 const passport			= require('passport');
-const passport			= require('passport');
 const app				= express();
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 const bcrypt			= require('bcrypt');
 
-mongoose.connect("mongodb://localhost:27017/node-auth-yt", {
+const {isLoggedIn} = require('./middleware/logCheck');
+const {isLoggedOut} = require('./middleware/logCheck');
+//routes
+const dashboardRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+// Connect to database
+mongoose.connect("mongodb://localhost:27017", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
 
 
-
+// Middleware
 app.set('view engine', 'ejs');
-app.set('views',__dirname+'/views')
-app.set('layout','layouts/layout')
+app.set('views',__dirname+'/views');
+app.set('layout','layouts/layout');
+
+// Routes
+app.set('/',dashboardRouter);
+app.set('/login',loginRouter);
 
 app.use(expressLayouts)
 app.use(methodOverride('_method'))
@@ -24,7 +34,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
-
+// Run the application at any port
 app.listen(5468, () => {
 	console.log("Listening on port 5468");
 });
---
+++ webComponent/routes/index.js
@@ -0,0 +1,14 @@
+const {isLoggedIn} = require('../middleware/logCheck')
+const {isLoggedOut} = require('../middleware/logCheck')
+
+const express = require('express')
+const User = require('../model/User');
+const router = express.Router();
+const passport			= require('passport');
+
+router.get('/', isLoggedIn, (req, res) => {
+	//console.log(req.user);
+	res.render("index", { title: req.user.username });
+});
+
+module.exports = router
